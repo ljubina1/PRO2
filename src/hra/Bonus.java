e5097ef438ed83b88ff0e58384da6c291be2f3de
ljubina1 <ljubina1@J08-19.uhk.cz>
2016-12-21 12:43:42 +0100

    zacatek bonusu

--- src/hra/Bonus.java
+++ src/hra/Bonus.java
@@ -1,6 +1,7 @@
 package hra;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
@@ -15,6 +16,12 @@ public class Bonus {
 	private int x;
 	private int y;
 	
+	public Bonus(BufferedImage img){
+		this.img = img;
+		random = new Random();
+		x= -200;
+		x=-200;
+	}
 	public void paint(Graphics g){
 		g.drawImage(img, x, y, null);
 		
@@ -22,6 +29,26 @@ public class Bonus {
 	public void posun(){
 		x = x + Zed.RYCHLOST;
 		//TODO
+		if(x<=0){
+			nastavNovyBonus();
+		}
+	}
+	public void nastavNovyBonus(){
+		vygenerujNahodneHondnotyProBonus();
+	}
+	public Rectangle getMez(){
+		return new Rectangle(x,y,SIRKA,VYSKA);
+	}
+	private void vygenerujNahodneHondnotyProBonus(){
+		x=HraciPlocha.SIRKA + (HraciPlocha.SIRKA / 2);
+		y = random.nextInt(HraciPlocha.VYSKA - 100);
+		
+	}
+	public boolean isBonusVygenerovaniDoZdi(Zed zed){
+		return( zed.getMezSpodniZdi().intersects(this.getMez())) ||
+				(zed.getMezHorniCastiZdi().intersects(this.getMez()));
+		
+		
 	}
 	
 }
